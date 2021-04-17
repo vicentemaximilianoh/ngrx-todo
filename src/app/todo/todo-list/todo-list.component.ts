cad4a45c070bcaf88625f598ea912046b60439ab
Maximiliano Vicente <vicente.maximilianoh@gmail.com>
2021-04-17 16:50:27 -0300

    Added hardcorde todo list

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.sass']
 })
 export class TodoListComponent implements OnInit {
+  public todoText = '';
+  public todoList = [
+    {id: 1, text: 'test1'},
+    {id: 2, text: 'test2'},
+    {id: 3, text: 'test3'}
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
